10337693fa7c57a88686786420a32783f57aebec
charles-m-doan <cmatthewdoan@gmail.com>
2019-07-18 11:24:56 -0400

    Initial build for backend API.

---
+++ src/main/java/wcci/swnb/StarsWithoutNumberBackendApplication.java
@@ -0,0 +1,13 @@
+package wcci.swnb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarsWithoutNumberBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarsWithoutNumberBackendApplication.class, args);
+	}
+
+}
